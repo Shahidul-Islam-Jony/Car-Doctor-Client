2a4a948cd45427cecff58ffdd715c74adcb10e55
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-10-24 22:31:00 +0600

    M(58-7) Create a simple Login page with Firebase integration

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,7 +1,8 @@
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import PropTypes from 'prop-types';
 
-
-const ServiceCard = ({service}) => {
-    const {title,img,price} = service;
+const ServiceCard = ({ service }) => {
+    const { title, img, price } = service;
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
@@ -9,13 +10,17 @@ const ServiceCard = ({service}) => {
             </figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
-                <p className="text-3xl text-orange-500 ">Price : ${price}</p>
-                <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+                <div className="flex justify-between items-center">
+                    <p className="text-3xl text-orange-500 ">Price : ${price}</p>
+                    <b className="text-2xl text-orange-600"><AiOutlineArrowRight></AiOutlineArrowRight></b>
                 </div>
             </div>
         </div>
     );
 };
+
+ServiceCard.propTypes = {
+    service: PropTypes.object,
+}
 
 export default ServiceCard;
