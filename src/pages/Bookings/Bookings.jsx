2d725db62ecd92b7959c9f49422cda9c5dd49a5b
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-10-26 23:22:06 +0600

    M(59-7) Display bookings and service image in a tabular data

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,22 +1,48 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import BookingRow from "./BookingRow";
 
 
 const Bookings = () => {
-    const {user} = useContext(AuthContext);
-    const [bookings,setBookings] = useState([]);
+    const { user } = useContext(AuthContext);
+    const [bookings, setBookings] = useState([]);
 
-    const url = `http://localhost:5000/bookings?email=${user.email}`;
+    const url = `http://localhost:5000/bookings?email=${user?.email}`;
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch(url)
-        .then(res=>res.json())
-        .then(data=>console.log(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setBookings(data))
+    }, [])
 
     return (
         <div>
-            
+            <h2 className="text-5xl">Your Bookings : {bookings.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       {
+                        bookings.map(booking=><BookingRow key={booking._id} booking={booking}>
+                        </BookingRow>)
+                       }
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
