256dfa0a3dcaf93e0c65fd83e9a83a6101ed2f25
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-10-27 20:37:06 +0600

    M(60-2) Auth Redirect using useLocation

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,11 +1,14 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 
 
 const PrivateRoute = ({children}) => {
     const {user,loading} = useContext(AuthContext);
+    const location = useLocation();
+    // console.log(location);
+
 
     if(loading){
         return <progress className="progress w-56"></progress>
@@ -15,7 +18,7 @@ const PrivateRoute = ({children}) => {
         return children;
     }
 
-    return <Navigate to="/login" replace></Navigate>
+    return <Navigate to="/login" state={location.pathname} replace></Navigate>
 };
 
 PrivateRoute.propTypes = {
